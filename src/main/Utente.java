5576b0bfe450e0397f8ae94dc5a9d64c262093e9
Manuel <emanu.carrino@studenti.unina.it>
2025-09-29 17:09:38 +0200

    Prova 1

---
+++ src/main/Utente.java
@@ -0,0 +1,82 @@
+package main;
+
+import java.util.Objects;
+
+public class Utente {
+    private String nome;
+    private String cognome;
+    private String matricola;
+    private String username;
+    private String password;
+    private String dataNascita;
+    private String genere;
+
+    public Utente() {
+    }
+
+    public Utente(String nome,String cognome,String matricola,String username,String password,String dataNascita,String genere) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.matricola = matricola;
+        this.username = username;
+        this.password = password;
+        this.dataNascita = dataNascita;
+        this.genere = genere;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getMatricola() {
+        return matricola;
+    }
+
+    public void setMatricola(String matricola) {
+        this.matricola = matricola;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(String dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+
+}
