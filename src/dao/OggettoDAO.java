45895a3a467a8c6901c4fa1070b0dc0a47390d1d
nicolaaa55 <nicoascione15@gmail.com>
2025-10-01 22:59:59 +0200

    OggettoDAO: Read completato con ordinamenti

--- src/dao/OggettoDAO.java
+++ src/dao/OggettoDAO.java
@@ -3,8 +3,11 @@ package dao;
 import dto.OggettoDTO;
 import dto.UtenteDTO;
 import dto.AnnuncioDTO;
+import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.ArrayList;
 
 import dto.DB_Connection;
 import java.sql.SQLException;
@@ -38,8 +41,112 @@ public class OggettoDAO {
         }
     }
 
+    //Ricerca per nome esatto
+    public List<OggettoDTO> getOggettiByNome(String nome){
+        String sql = """
+                SELECT *
+                FROM Oggetto
+                WHERE Nome = ?
+                """;
+        ArrayList<OggettoDTO> risultato = new ArrayList<>();
+        try (Connection con = getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+                ps.setString(1, nome);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()){
+                    String id = rs.getString("ID_Oggetto");
+                    int numProp = rs.getInt("numProprietari");
+                    String condizioni = rs.getString("Condizioni");
+                    String dimensione = rs.getString("Dimensione");
+                    Float peso = rs.getFloat("Peso_Kg");
+                    String proprietario = rs.getString("FK_Utente");
+                    risultato.add(new OggettoDTO(id, nome, numProp, condizioni, dimensione, peso, proprietario));
+                }
+        } catch (SQLException e){
+            throw new RuntimeException("Errore nella ricerca per nome: " + nome, e);
+        }
+        return risultato;
+    }
+
+    //Ricerca per ID Oggetto
+    public OggettoDTO getOggettiById(String id){
+        String sql = """
+                SELECT *
+                FROM Oggetto
+                WHERE ID_Oggetto = ?
+                """;
+        OggettoDTO risultato = null;
+        try (Connection con = getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+                ps.setString(1, id);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()){
+                    String nomeOgg = rs.getString("Nome");
+                    int numProp = rs.getInt("numProprietari");
+                    String condizioni = rs.getString("Condizioni");
+                    String dimensione = rs.getString("Dimensione");
+                    Float peso = rs.getFloat("Peso_Kg");
+                    String proprietario = rs.getString("FK_Utente"); 
+                    risultato = new OggettoDTO(id, nomeOgg, numProp, condizioni, dimensione, peso, proprietario);
+                }
+        } catch (SQLException e){
+            throw new RuntimeException("Errore nella ricerca per ID: " + id, e);
+        }
+        return risultato;
+    }
+
+    //Ricerca ordinata per peso crescente
+    public List<OggettoDTO> orderOggettiByPeso(){
+        String sql = """
+                SELECT *
+                FROM Oggetto
+                ORDER BY Peso_Kg ASC
+                """;
+        ArrayList<OggettoDTO> risultato = new ArrayList<>();
+        try (Connection con = getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()){
+                    String id = rs.getString("ID_Oggetto");
+                    String nome = rs.getString("Nome");
+                    int numProp = rs.getInt("numProprietari");
+                    String condizioni = rs.getString("Condizioni");
+                    String dimensione = rs.getString("Dimensione");
+                    Float peso = rs.getFloat("Peso_Kg");
+                    String proprietario = rs.getString("FK_Utente");
+                    risultato.add(new OggettoDTO(id, nome, numProp, condizioni, dimensione, peso, proprietario));
+                }
+        } catch (SQLException e){
+            throw new RuntimeException("Errore nell'ordinamento per peso.");
+        }
+        return risultato;
+    }
+
+    //Ricerca ordinata per numero di proprietari
+    public List<OggettoDTO> orderOggettiByNumPropr(){
+        String sql = """
+                SELECT *
+                FROM Oggetto
+                ORDER BY numProprietari ASC
+                """;
+        ArrayList<OggettoDTO> risultato = new ArrayList<>();
+        try (Connection con = getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()){
+                    String id = rs.getString("ID_Oggetto");
+                    String nome = rs.getString("Nome");
+                    int numProp = rs.getInt("numProprietari");
+                    String condizioni = rs.getString("Condizioni");
+                    String dimensione = rs.getString("Dimensione");
+                    Float peso = rs.getFloat("Peso_Kg");
+                    String proprietario = rs.getString("FK_Utente");
+                    risultato.add(new OggettoDTO(id, nome, numProp, condizioni, dimensione, peso, proprietario));
+                }
+        } catch (SQLException e){
+            throw new RuntimeException("Errore nell'ordinamento per numero di proprietari.");
+        }
+        return risultato;
+    }
+
     //Controllo necessario per l'utilizzo di un oggetto
-    private void validazioneOggetto(OggettoDTO o) {
+    public void validazioneOggetto(OggettoDTO o) {
         if (o == null) {
             throw new IllegalArgumentException("Oggetto null");
         }
@@ -66,4 +173,5 @@ public class OggettoDAO {
         }
     }
 
+    
 }
