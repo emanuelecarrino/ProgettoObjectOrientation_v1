c2e3cc0e298768cd96c8eb60affa0dd72f3f6ce4
Manuel <emanu.carrino@studenti.unina.it>
2025-10-04 13:16:39 +0200

    committino

--- src/frames/LoginWindow.java
+++ src/frames/LoginWindow.java
@@ -28,12 +28,10 @@ public class LoginWindow extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	
 	public LoginWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(120, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
